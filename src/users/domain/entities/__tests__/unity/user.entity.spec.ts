29cb6c5deaaef70fea2f3619c229400479ce3b0d
andre.borba <dev_andre.filipe@outlook.com>
2023-09-17 01:43:33 -0300

    Add getters and types unit tests

--- src/users/domain/entities/__tests__/unity/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unity/user.entity.spec.ts
@@ -20,4 +20,23 @@ describe('UserEntity', () => {
     expect(sut.props.password).toEqual(props.password)
     expect(sut.props.createdAt).toBeInstanceOf(Date)
   })
+
+  it('should be defined (getters)', () => {
+    expect(sut.name).toEqual(props.name)
+    expect(sut.email).toEqual(props.email)
+    expect(sut.password).toEqual(props.password)
+  })
+
+  it('should be defined', () => {
+    expect(sut).toBeDefined()
+  })
+
+  it('should be correctly types', () => {
+    expect(sut).toBeInstanceOf(UserEntity)
+    expect(typeof sut.props.name).toBe('string')
+    expect(typeof sut.props.email).toBe('string')
+    expect(typeof sut.props.password).toBe('string')
+    expect(sut.props.createdAt).toBeInstanceOf(Date)
+    expect(sut.props.updatedAt).toBeInstanceOf(Date)
+  })
 })
